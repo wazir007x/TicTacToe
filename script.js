f380529991c7f2b4270900f0b7b3b6b0c82178a3
wazir007x <abhipdgupta007@gmail.com>
2022-12-08 16:07:19 +0530

    added line

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ let result = document.getElementById("result");
 let board = ["", "", "", "", "", "", "", "", ""];
 
 let blocks = document.querySelectorAll(".block");
+let line = document.getElementsByClassName("line")[0];
 
 document.getElementById("reset").onclick = () => {
   currentplayer = player[0];
   currentplayerindex = 0;
-  GO=false
+  
+  line.style = `top:50%;opacity:0`;
+  line.classList.remove("line_animate");
+  
+  GO = false;
   board = ["", "", "", "", "", "", "", "", ""];
   blocks.forEach((block) => {
     block.innerHTML = `<p></p>`;
@@ -29,6 +34,7 @@ blocks.forEach((block, index) => {
       block.innerHTML = `<p>${currentplayer}</p>`;
 
       if (winner() == true) {
+        line.classList.add("line_animate");
         result.innerHTML = `<h1>WINNER IS ${currentplayer}</h1>`;
 
         document.getElementById("reset").innerHTML = "<h1>Play Again</h1>";
@@ -49,24 +55,45 @@ blocks.forEach((block, index) => {
 });
 
 const winner = () => {
-  if (board[0] != "" && board[0] == board[1] && board[0] == board[2])
+  if (board[0] != "" && board[0] == board[1] && board[0] == board[2]) {
+    line.style = `transform: translateY(-180px);`;
     return true;
-  if (board[3] != "" && board[3] == board[4] && board[3] == board[5])
+  }
+  if (board[3] != "" && board[3] == board[4] && board[3] == board[5]) {
+    line.style=`top:50%`;
     return true;
-  if (board[6] != "" && board[6] == board[7] && board[6] == board[8])
+  }
+  if (board[6] != "" && board[6] == board[7] && board[6] == board[8]) {
+    line.style = `transform: translateY(180px);`;
     return true;
+  }
 
-  if (board[0] != "" && board[0] == board[3] && board[0] == board[6])
+  if (board[0] != "" && board[0] == board[3] && board[0] == board[6]) {
+    line.style = `
+    transform:translateX(-180px) rotate(90deg);
+     `;
     return true;
-  if (board[1] != "" && board[1] == board[4] && board[1] == board[7])
-    return true;
-  if (board[2] != "" && board[2] == board[5] && board[2] == board[8])
-    return true;
+  }
 
-  if (board[0] != "" && board[0] == board[4] && board[0] == board[8])
+  if (board[1] != "" && board[1] == board[4] && board[1] == board[7]) {
+    line.style = `transform:rotate(90deg);`;
     return true;
-  if (board[2] != "" && board[2] == board[4] && board[2] == board[6])
+  }
+
+  if (board[2] != "" && board[2] == board[5] && board[2] == board[8]) {
+    line.style = `transform:translateX(180px) rotate(90deg);`;
     return true;
+  }
+
+  if (board[0] != "" && board[0] == board[4] && board[0] == board[8]) {
+    line.style = `transform: rotate(45deg);`;
+    return true;
+  }
+
+  if (board[2] != "" && board[2] == board[4] && board[2] == board[6]) {
+    line.style = `transform: rotate(-45deg)`;
+    return true;
+  }
 };
 
 const gameover = () => {
